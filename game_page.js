0a465637188fec014de36ff1efafe25ea73ea675
Abhi11235 <117432986+Abhi11235@users.noreply.github.com>
2023-06-30 16:09:50 +1000

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,9 @@
+function send(){
+number1=document.getElementById("word_1").value;
+number2=document.getElementById("word_2").value; 
+actual_answer+parsInt(number1)* parsInt(number2)
+}
+
+question_number="<h4>"+nmber1  +"X" +number2 + "</h4>"
+
+input_box ="<br>Answer:<input type=text id=input_check_box>";
